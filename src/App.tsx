6c14acc99e50154c1b871b21005ddcda1bdc5b85
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-08-22 17:40:21 -0300

    Feat: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Helloooooooooooo</h1>
+      <Transactions />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
