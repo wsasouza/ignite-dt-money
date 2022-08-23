b574ee76a2d5b26cb376db3189b76af9b131fbc7
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-08-22 23:25:10 -0300

    Feat: Create Modal Transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
 export function Header() {
@@ -6,7 +9,13 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
