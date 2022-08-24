de3b64795791cedcaf7e6c22ea2aa6fe9d4d3a7c
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-08-24 15:40:30 -0300

    Refactor: Search Form with memo, only test

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
@@ -14,7 +15,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -48,3 +49,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
